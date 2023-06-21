08541b778d977111b4040b791818838f46a100f6
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-21 13:07:04 -0400

    Squares now darken gradually if hovered over repeatedly

--- script.js
+++ script.js
@@ -1,70 +1,79 @@
 function sizeSketchPad() {
+    // Scales the sketchpad area based off the current viewport size
+    // Updates the sketchPadLength global var for use in the addGrid() function
     let bodyHeight = parseInt(window.getComputedStyle(body).height) * .75;
     let bodyWidth = parseInt(window.getComputedStyle(body).width);
     if (bodyHeight >= bodyWidth) {
-        sideLength += bodyWidth
+        sketchPadLength = bodyWidth
         sketchPad.style.width = bodyWidth + 'px';
         sketchPad.style.height = bodyWidth + 'px';
     } else {
-        sideLength += bodyHeight
+        sketchPadLength = bodyHeight
         sketchPad.style.width = bodyHeight + 'px';
         sketchPad.style.height = bodyHeight + 'px';
     }
-    console.log(bodyWidth)
-    console.log(sketchPad.style.width)
     return;
 }
 
-function addGrid(rows, columns) {
-    while ((rows > 100) || (columns > 100)) {
-        alert('# of rows and/or columns exceeding 100 may cause performance issues. Please try again.')
-        rows = parseInt(prompt('Enter # of Rows (max 100):'));
-        columns = parseInt(prompt('Enter # of Columns (max 100):'))
+function addGrid(squaresPerSide) {
+    // Adds a grid with (squaresPerSide) rows and columns
+    while ((squaresPerSide < 0) || (squaresPerSide > 100)) {
+        alert('ERROR: Input must be non-negative and less than 100. Please try again.')
+        squaresPerSide = parseInt(prompt('Enter size of grid (max 100):'));
     }
-    for (let a = 1; a <= rows; a++) {
+    for (let a = 1; a <= squaresPerSide; a++) {
         const newRow = document.createElement('div');
         newRow.classList.add('row');
         sketchPad.appendChild(newRow);
-        for (let b = 1; b <= columns; b++) {
-            const newRectangle = document.createElement('div');
-            newRectangle.classList.add('rectangle');
-            let widthOfRectangle = sideLength / columns;
-            let heightOfRectangle = sideLength / rows;
-            newRectangle.style.width = widthOfRectangle + 'px';
-            newRectangle.style.height = heightOfRectangle + 'px';
-            newRectangle.style.backgroundColor = 'white';
-            newRow.appendChild(newRectangle);
+        for (let b = 1; b <= squaresPerSide; b++) {
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('square');
+            let sideOfSquare = sketchPadLength / squaresPerSide;
+            newSquare.style.width = sideOfSquare + 'px';
+            newSquare.style.height = sideOfSquare + 'px';
+            newSquare.style.backgroundColor = 'white';
+            newSquare.style.filter = 'brightness(100%)';
+            newRow.appendChild(newSquare);
         };
     };
     return;
 };
 
 function randomRGB() {
+    // returns a randomized RGB code for use in the reset() function
     let colorCode = 'rgb(';
-    colorCode += Math.round(Math.random() * 100) + '%, ';
-    colorCode += Math.round(Math.random() * 100) + '%, ';
-    colorCode += Math.round(Math.random() * 100) + '%)';
+    colorCode += (Math.random() * 100) + '%, ';
+    colorCode += (Math.random() * 100) + '%, ';
+    colorCode += (Math.random() * 100) + '%)';
     return(colorCode);
 };
 
 function reset() {
+    // removes all existing elements within sketchpad and creates a new grid
+    // adds new event listener for each square
     while (sketchPad.firstChild) {
         sketchPad.removeChild(sketchPad.firstChild);
     };
-    addGrid(parseInt(prompt('Enter # of Rows (max 100):')), parseInt(prompt('Enter # of Columns (max 100):')));
-    const rectangles = document.querySelectorAll('.rectangle');
-    rectangles.forEach((div) => {
+    addGrid(parseInt(prompt('Enter size of grid (max 100):')));
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((div) => {
         div.addEventListener('mouseover', () => {
             if (div.style.backgroundColor === 'white') {
                 div.style.backgroundColor = randomRGB()
+            } else {
+                currentFilterLength = div.style.filter.length;
+                currentFilter = parseInt(div.style.filter.slice(11, currentFilterLength - 2));
+                if (currentFilter > 0) {
+                    div.style.filter = 'brightness(' + (currentFilter - 10) + '%)';
+                    console.log(div.style.filter);
+                };
             };
         });
     });
     return;
 };
 
-currentColor = 'black'
-sideLength = 0
+sketchPadLength = 0
 const body = document.querySelector('body');
 const resetButton = document.getElementById('resetbutton');
 const sketchPad = document.getElementById('sketchpad');
