c847de4052cff32e0b1f7d53d7a39a04087e9e98
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-21 10:51:25 -0400

    Added button for resetting grid and square colors are now randomized

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function sizeSketchPad() {
-    let bodyHeight = parseInt(window.getComputedStyle(body).height);
+    let bodyHeight = parseInt(window.getComputedStyle(body).height) * .9;
     let bodyWidth = parseInt(window.getComputedStyle(body).width);
     if (bodyHeight >= bodyWidth) {
         sideLength += bodyWidth
         sketchPad.style.width = bodyWidth + 'px';
         sketchPad.style.height = bodyWidth + 'px';
+        resetButton.style.width = bodyWidth + 'px';
     } else {
         sideLength += bodyHeight
         sketchPad.style.width = bodyHeight + 'px';
         sketchPad.style.height = bodyHeight + 'px';
+        resetButton.style.width = bodyHeight + 'px';
     }
     console.log(bodyWidth)
     console.log(sketchPad.style.width)
@@ -16,6 +18,11 @@ function sizeSketchPad() {
 }
 
 function addGrid(rows, columns) {
+    while ((rows > 100) || (columns > 100)) {
+        alert('# of rows and/or columns exceeding 100 may cause performance issues. Please try again.')
+        rows = parseInt(prompt('Enter # of Rows (max 100):'));
+        columns = parseInt(prompt('Enter # of Columns (max 100):'))
+    }
     for (let a = 1; a <= rows; a++) {
         const newRow = document.createElement('div');
         newRow.classList.add('row');
@@ -27,23 +34,46 @@ function addGrid(rows, columns) {
             let heightOfRectangle = sideLength / rows;
             newRectangle.style.width = widthOfRectangle + 'px';
             newRectangle.style.height = heightOfRectangle + 'px';
-            newRectangle.style.outline = '.5px solid black';
+            newRectangle.style.backgroundColor = 'white';
             newRow.appendChild(newRectangle);
         };
     };
     return;
 };
 
+function randomRGB() {
+    let colorCode = 'rgb(';
+    colorCode += Math.round(Math.random() * 100) + '%, ';
+    colorCode += Math.round(Math.random() * 100) + '%, ';
+    colorCode += Math.round(Math.random() * 100) + '%)';
+    return(colorCode);
+};
+
+function reset() {
+    while (sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
+    };
+    addGrid(parseInt(prompt('Enter # of Rows (max 100):')), parseInt(prompt('Enter # of Columns (max 100):')));
+    const rectangles = document.querySelectorAll('.rectangle');
+    rectangles.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            if (div.style.backgroundColor === 'white') {
+                div.style.backgroundColor = randomRGB()
+            };
+        });
+    });
+    return;
+};
+
 currentColor = 'black'
 sideLength = 0
 const body = document.querySelector('body');
+const resetButton = document.getElementById('resetbutton');
 const sketchPad = document.getElementById('sketchpad');
+const r = document.querySelectorAll('row');
 sizeSketchPad()
-addGrid(16, 16)
-const rectangles = document.querySelectorAll('.rectangle');
-console.log(rectangles)
-rectangles.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = currentColor
-    });
+reset()
+
+resetButton.addEventListener('click', () => {
+    reset();
 });
